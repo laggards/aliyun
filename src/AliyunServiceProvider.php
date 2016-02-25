a69d17e2609e39f90be9537502a74825b8d59406
laggards <laggards@foxmail.com>
2016-02-25 16:26:12 +0800

    support lumen framework

--- src/AliyunServiceProvider.php
+++ src/AliyunServiceProvider.php
@@ -1,7 +1,10 @@
 <?php
 namespace Laggards\Aliyun;
 
+use Illuminate\Contracts\Container\Container as Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 class AliyunServiceProvider extends ServiceProvider
 {
@@ -12,13 +15,27 @@ class AliyunServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $source = realpath(__DIR__ . '/../config/aliyun.php');
-        if (class_exists('Illuminate\Foundation\Application', false)) {
-            $this->publishes([$source => config_path('aliyun.php')]);
-        }
-        $this->mergeConfigFrom($source, 'aliyun');
+		$this->setupConfig($this->app);
     }
 
+	/**
+     * Setup the config.
+     *
+     * @param \Illuminate\Contracts\Container\Container $app
+     *
+     * @return void
+     */
+    protected function setupConfig(Application $app)
+    {
+		$source = realpath(__DIR__ . '/../config/aliyun.php');
+		if ($app instanceof LaravelApplication && $app->runningInConsole()) {
+            $this->publishes([$source => config_path('aliyun.php')]);
+        } elseif ($app instanceof LumenApplication) {
+            $app->configure('aliyun');
+        }
+		$this->mergeConfigFrom($source, 'aliyun');
+	}
+	
     /**
      * Register the application services.
      *

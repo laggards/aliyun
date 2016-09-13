99df4e0c7cffd647082c55201061b2befb73c6a3
laggards <laggards@hotmail.com>
2016-09-13 16:00:22 +0800

    update aliyuncs oss sdk

--- src/AliyunFacade.php
+++ src/AliyunFacade.php
@@ -1,9 +1,10 @@
 <?php namespace Laggards\Aliyun;
 
-use Laggards\Aliyun\OSS;
 use Laggards\Aliyun\MNS;
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Config\Repository;
+use OSS\OssClient;
+use OSS\Core\OssException;
 
 /**
  * Facade for the Aliyun service
@@ -15,27 +16,24 @@ class AliyunFacade extends Facade
 	public function __construct()
     {
         $this->app = app();
-
         $this->configRepository = config('aliyun');;
     }
 	
 	public static function createClient($obj){
 		switch($obj){
 			case 'oss':
-				$oss = new OSS();
-				return $oss;
+				$client = new OssClient(config('aliyun.oss.AccessKeyId'), config('aliyun.oss.AccessKeySecret'), config('aliyun.oss.Endpoint'));
 				break;
 			case 'mns':
-				$mns = new MNS();
-				return $mns;
+				$client = new MNS();
 				break;
 			case 'memcache':
-				$memcache = new MemcacheSASL();
-				return $memcache;
+				$client = new MemcacheSASL();
 				break;
 			default:
 				return null;
 				break;	
-		}	
+		}
+		return $client;
 	}
 }
